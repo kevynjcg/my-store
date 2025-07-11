d5ad7e837c82a20b8a93e031ce3a6bdfdacaaae8
Kevyn <59120183+kevynjcg@users.noreply.github.com>
2025-07-11 18:11:26 +0800

    Update wallet.py

--- src/db/models/wallet.py
+++ src/db/models/wallet.py
@@ -5,10 +5,10 @@ from src.db.session import Base
 
 class Wallet(Base):
     __tablename__ = "wallets"
-    __table_args__ = {"schema": "kevyn_store"}
+
     
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("kevyn_store.users.id", ondelete="CASCADE"), unique=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), unique=True)
     balance = Column(Numeric(10, 2), default=0.00)
     updated_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), onupdate=func.now())
 
