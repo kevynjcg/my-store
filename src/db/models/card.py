dc1947f39db9ce68b071803c78575fb29b3b87a2
Kevyn <59120183+kevynjcg@users.noreply.github.com>
2025-07-11 18:10:17 +0800

    Update card.py

--- src/db/models/card.py
+++ src/db/models/card.py
@@ -5,7 +5,7 @@ from src.db.session import Base
 
 class Card(Base):
     __tablename__ = "cards"
-    __table_args__ = {"schema": "kevyn_store"}
+
 
     id = Column(Integer, primary_key=True, index=True)
     card_number = Column(String(16))  
@@ -15,6 +15,6 @@ class Card(Base):
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
     first_name = Column(String) 
     last_name = Column(String) 
-    user_id = Column(Integer, ForeignKey("kevyn_store.users.id", ondelete="CASCADE"), unique=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), unique=True)
 
     user = relationship("User", backref="card")
